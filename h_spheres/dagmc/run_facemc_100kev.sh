ecb4ebd01ce8a0e540ecff1310f1196d4351432c
Luke Kersting <lkersting@wisc.edu>
2016-06-20 15:48:19 -0600

    Added run scripts that use frensie's test_files cross_section.xml

---
+++ h_spheres/dagmc/run_facemc_100kev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##---------------------------------------------------------------------------##
+## FACEMC test runner
+##---------------------------------------------------------------------------##
+
+# Run Facemc
+./facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_100kev.xml --src_def=h_spheres_source.xml --cross_sec_dir=/home/lkersting/frensie/src/packages/test_files --threads=12
