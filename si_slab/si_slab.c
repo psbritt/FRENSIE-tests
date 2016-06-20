e41e6bdf95beaebd7f4cd649939924dbdc9d020d
Luke Kersting <lkersting@wisc.edu>
2016-06-20 12:35:54 -0600

    removed unwanted files

--- si_slab/si_slab.c
+++ si_slab/si_slab.c
@@ -1,8 +1,8 @@
 //---------------------------------------------------------------------------//
 //!
 //! \file   si_slab.c
-//! \author Alex Robinson
-//! \brief  Geometry for Neutron H-1 sphere at 2500K verification problem
+//! \author Luke Kersting
+//! \brief  Geometry for Electron Si-Slab verification problem
 //!
 //---------------------------------------------------------------------------//
 
@@ -29,24 +29,24 @@ void h_sphere()
 
   // Create the silicon volume
   TGeoVolume* si_slab_volume =
-    geom->MakeSphere( "CUBE", med_1, 5.0, 5.0, 0.00025 );
+    geom->MakeSphere( "CUBE", med_1, 5.0, 5.0, 0.0002 );
 
   si_slab_volume->SetUniqueID( 1 );
 
   // Create the void volume
   TGeoVolume* void_cube_volume = geom->MakeBox(
-                                             "CUBE", void_med, 6.0, 6.0, 3.0 );
+                                             "CUBE", void_med, 2.55, 2.55, 2.0 );
 
   void_cube_volume->SetUniqueID( 2 );
 
   // Create the graveyard volume
   TGeoVolume* graveyard_volume = geom->MakeBox(
-                                   "GRAVEYARD", graveyard_med, 7.0, 7.0, 4.0 );
+                                   "GRAVEYARD", graveyard_med, 3.5, 3.5, 3.5 );
 
   graveyard_volume->SetUniqueID( 3 );
 
   // Place the silicon slab inside of the void cube
-  void_cube_volume->AddNode( si_slab_volume, 1, new TGeoTranslation(0.0,0.0,0.1) );
+  void_cube_volume->AddNode( si_slab_volume, 1 );
 
   // Place the void cube inside of the graveyard
   graveyard_volume->AddNode( void_cube_volume, 1 );
