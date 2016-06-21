436634a14ca78b1963f2b5ca471cb08e8f852339
Luke Kersting <lkersting@wisc.edu>
2016-06-21 11:25:01 -0600

    updated the xml files to the new syntax

--- h_spheres/dagmc/run_facemc_1kev.sh
+++ h_spheres/dagmc/run_facemc_1kev.sh
@@ -3,5 +3,9 @@
 ## FACEMC test runner
 ##---------------------------------------------------------------------------##
 
+# Set data directory path.
+#echo -n "Enter cross_section.xml directory: > "
+#read cross_section_directory
+
 # Run Facemc
-../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_1kev.xml --src_def=h_spheres_source.xml --cross_sec_dir=/home/lkersting/frensie/src/packages/test_files --threads=12
+../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_1kev.xml --src_def=h_spheres_source.xml --cross_sec_dir=/home/ljkerst/software/frensie/src/packages/test_files --threads=12
