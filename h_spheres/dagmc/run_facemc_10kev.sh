1cb6837b54ed791807ab4369e7a8726e0f23f850
Luke Kersting <lkersting@wisc.edu>
2016-06-21 15:59:04 -0600

    updated H Spheres test files and added a README file to the Si Slab Test

--- h_spheres/dagmc/run_facemc_10kev.sh
+++ h_spheres/dagmc/run_facemc_10kev.sh
@@ -3,5 +3,9 @@
 ## FACEMC test runner
 ##---------------------------------------------------------------------------##
 
+# Set cross_section.xml directory path.
+EXTRA_ARGS=$@
+CROSS_SECTION_XML_PATH=/home/ljkerst/software/frensie/src/packages/test_files
+
 # Run Facemc
-../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_10kev.xml --src_def=h_spheres_source.xml --cross_sec_dir=/home/lkersting/frensie/src/packages/test_files --threads=12
+../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_10kev.xml --src_def=h_spheres_source_10kev.xml --cross_sec_dir=$CROSS_SECTION_XML_PATH --threads=12
--- h_spheres/dagmc/run_facemc_1kev.sh
+++ h_spheres/dagmc/run_facemc_1kev.sh
@@ -3,9 +3,9 @@
 ## FACEMC test runner
 ##---------------------------------------------------------------------------##
 
-# Set data directory path.
-#echo -n "Enter cross_section.xml directory: > "
-#read cross_section_directory
+# Set cross_section.xml directory path.
+EXTRA_ARGS=$@
+CROSS_SECTION_XML_PATH=/home/ljkerst/software/frensie/src/packages/test_files
 
 # Run Facemc
-../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_1kev.xml --src_def=h_spheres_source.xml --cross_sec_dir=/home/ljkerst/software/frensie/src/packages/test_files --threads=12
+../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_1kev.xml --src_def=h_spheres_source_1kev.xml --cross_sec_dir=$CROSS_SECTION_XML_PATH --threads=12
