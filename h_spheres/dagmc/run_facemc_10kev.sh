2c7241b2a0e8ce756add0098e2e27a5497baf659
Luke Kersting <lkersting@wisc.edu>
2016-07-11 16:32:26 -0600

    added other things to the sim info

--- h_spheres/dagmc/run_facemc_10kev.sh
+++ h_spheres/dagmc/run_facemc_10kev.sh
@@ -8,4 +8,4 @@ EXTRA_ARGS=$@
 CROSS_SECTION_XML_PATH=/home/ljkerst/software/frensie/src/packages/test_files
 
 # Run Facemc
-../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_10kev.xml --src_def=h_spheres_source_10kev.xml --cross_sec_dir=$CROSS_SECTION_XML_PATH --threads=12
+../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_10kev.xml --src_def=h_spheres_source_10kev.xml --cross_sec_dir=$CROSS_SECTION_XML_PATH --simulation_name="h_spheres_10kev" --threads=12
--- h_spheres/dagmc/run_facemc_1kev.sh
+++ h_spheres/dagmc/run_facemc_1kev.sh
@@ -5,7 +5,7 @@
 
 # Set cross_section.xml directory path.
 EXTRA_ARGS=$@
-CROSS_SECTION_XML_PATH=/home/ljkerst/software/frensie/src/packages/test_files
+CROSS_SECTION_XML_PATH=/home/ljkerst/software/MCNP/ccc810/MCNP_DATA
 
 # Run Facemc
-../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_1kev.xml --src_def=h_spheres_source_1kev.xml --cross_sec_dir=$CROSS_SECTION_XML_PATH --threads=12
+../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_1kev.xml --src_def=h_spheres_source_1kev.xml --cross_sec_dir=$CROSS_SECTION_XML_PATH --simulation_name="h_spheres_1kev" --threads=12
