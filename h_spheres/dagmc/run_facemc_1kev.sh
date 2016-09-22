70231cca0a47c74347dddb19e2126a98b8a13352
Luke Kersting <lkersting@wisc.edu>
2016-09-22 14:39:48 -0500

    add .gitignore so run data is ignored. The run scripts have also been updated

--- h_spheres/dagmc/run_facemc_1kev.sh
+++ h_spheres/dagmc/run_facemc_1kev.sh
@@ -5,7 +5,22 @@
 
 # Set cross_section.xml directory path.
 EXTRA_ARGS=$@
-CROSS_SECTION_XML_PATH=/home/ljkerst/software/MCNP/ccc810/MCNP_DATA
+CROSS_SECTION_XML_PATH=/home/software/mcnpdata/
 
 # Run Facemc
 ../../../bin/facemc --sim_info=sim_info.xml --geom_def=h_spheres_geom.xml --mat_def=h_spheres_mat.xml --resp_def=h_spheres_rsp_fn.xml --est_def=h_spheres_est_1kev.xml --src_def=h_spheres_source_1kev.xml --cross_sec_dir=$CROSS_SECTION_XML_PATH --simulation_name="h_spheres_1kev" --threads=12
+
+# Extract the flux data
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 1 -i 1 > results/1kev_flux_1.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 1 -i 3 > results/1kev_flux_3.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 1 -i 6 > results/1kev_flux_6.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 1 -i 9 > results/1kev_flux_9.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 1 -i 12 > results/1kev_flux_12.txt
+
+# Extract the current data
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 2 -i 1 > results/1kev_current_1.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 2 -i 3 > results/1kev_current_3.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 2 -i 6 > results/1kev_current_6.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 2 -i 9 > results/1kev_current_9.txt
+../../../bin/edump.py -f h_spheres_1kev.h5 -e 2 -i 12 > results/1kev_current_12.txt
+
