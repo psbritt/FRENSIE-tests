13c8dd20a00132ef326812a69c2796f347da8b21
Luke Kersting <lkersting@wisc.edu>
2016-11-21 09:40:43 -0600

    Increased the density in the 100kev run so that the flux isn't so highly peaked near 100kev. Also added a cell track length flux tally to the dagmc runs.

--- h_spheres/mcnp/run_mcnp.sh
+++ h_spheres/mcnp/run_mcnp.sh
@@ -9,6 +9,13 @@ CROSS_SECTION_XML_PATH=/home/software/mcnpdata/
 TODAY=$(date +%Y-%m-%d)
 OUTPUT_DIR="./results/${TODAY}/"
 
+THREADS="8"
+if [ "$#" -eq 1 ];
+then
+    # Set the number of threads used
+    THREADS="$1"
+fi
+
 echo -n "Enter the energy to process in keV (1, 10, 100) > "
 read INPUT
 ENERGY="${INPUT}kev"
@@ -18,7 +25,7 @@ echo "You entered: $ENERGY"
 NAME="h_spheres_${ENERGY}.inp"
 
 echo "Running MCNP6:"
-/home/software/mcnp6.1.1/bin/mcnp611_linux_x86_64_omp n="$NAME" tasks 8
+/home/software/mcnp6.1.1/bin/mcnp611_linux_x86_64_omp n="$NAME" tasks ${THREADS}
 
 echo "Processing the results:"
 echo $INPUT | ./data_processor.py -d ${OUTPUT_DIR}
