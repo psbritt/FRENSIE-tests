33d037f4b9086fb6e9e104df757e19cdad7f7021
Luke Kersting <lkersting@wisc.edu>
2016-12-07 12:18:37 -0600

    changed time for mpi run

--- h_spheres/dagmc/run_facemc_mpi.sh
+++ h_spheres/dagmc/run_facemc_mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This file is named run_facemc_mpi.sh
 #SBATCH --partition=univ2
-#SBATCH --time=0-72:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --nodes=5
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem-per-cpu=4000
--- h_spheres/root/run_facemc_mpi.sh
+++ h_spheres/root/run_facemc_mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This file is named run_facemc_mpi.sh
 #SBATCH --partition=univ2                # Use the newer infrastructure
-#SBATCH --time=0-20:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem-per-cpu=4000
