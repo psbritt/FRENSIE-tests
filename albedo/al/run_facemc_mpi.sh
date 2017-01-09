9d04271df343053981ba7651b9feea59072b1465
Luke Kersting <lkersting@wisc.edu>
2017-01-09 11:57:14 -0600

    changed energy and updated mpi script

--- albedo/al/run_facemc_mpi.sh
+++ albedo/al/run_facemc_mpi.sh
@@ -69,7 +69,7 @@ GEOM="geom.xml"
 SOURCE="source.xml"
 RSP="../rsp_fn.xml"
 EST="../est.xml"
-NAME="al_${NAME}"
+NAME="al_2000_${NAME}"
 
 # Make directory for the test results
 TODAY=$(date +%Y-%m-%d)
@@ -78,7 +78,9 @@ mkdir -p $DIR
 
 THREADS="100"
 echo "Running Facemc with ${THREADS} threads:"
-mpiexec -n ${THREADS} ${FRENSIE}/bin/facemc --sim_info=${INFO} --geom_def=${GEOM} --mat_def=${MAT} --resp_def=$RSP --est_def=$EST --src_def=$SOURCE --cross_sec_dir=$CROSS_SECTION_XML_PATH --simulation_name=$NAME --threads=${THREADS} > ${DIR}/${NAME}.txt 2>&1
+RUN="mpiexec -n ${THREADS} ${FRENSIE}/bin/facemc-mpi --sim_info=${INFO} --geom_def=${GEOM} --mat_def=${MAT} --resp_def=${RSP} --est_def=${EST} --src_def=${SOURCE} --cross_sec_dir=${CROSS_SECTION_XML_PATH} --simulation_name=${NAME}"
+echo ${RUN}
+${RUN} > ${DIR}/${NAME}.txt 2>&1
 
 echo "Moving the results:"
 
