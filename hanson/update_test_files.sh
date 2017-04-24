d708d807631a571ffd19fa1af37f4b516b8063cd
Luke Kersting <lkersting@wisc.edu>
2017-04-24 14:46:49 -0500

    added script to update test files for Hanson Test

---
+++ hanson/update_test_files.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+##---------------------------------------------------------------------------##
+## Hanson test data updater
+##---------------------------------------------------------------------------##
+
+# Get the date for the table notes
+today=`date`
+notes="This table was generated on $today. It is for the Hanson electron test problem only!"
+
+# Set the data directory path.
+while getopts d: option
+do case "${option}"
+   in
+       d) cross_section_directory=${OPTARG};;
+   esac
+done
+
+if [ -d "$cross_section_directory" ]; then
+    
+    # Update Gold Log data
+    printf "Updating the Au LinLog native test data...\n"
+    epr_generator --cross_sec_dir=$cross_section_directory --cross_sec_alias=Au --min_photon_energy=1e-3 --max_photon_energy=20.0 --min_electron_energy=1e-5 --max_electron_energy=1e5 --occupation_num_tol=1e-3 --subshell_incoherent_tol=1e-3 --grid_convergence_tol=1e-4 --grid_absolute_diff_tol=1e-80 --grid_absolute_dist_tol=1e-18 --tabular_evaluation_tol=1e-15 --cutoff_angle_cosine=0.9 --number_of_moment_preserving_angles=2.0 --linlinlog_interp_on --modify_cs_xml_file --subdir="native" --output_alias="Native" --notes="$notes"
+    if [ $? -eq 0 ]
+    then
+        printf "Au native data updated successfully!\n\n"
+    else
+        printf "Au native data FAILED to update!\n"
+        exit 1
+    fi
+    
+    # Update Gold Lin data
+    printf "Updating the Au LinLin native test data...\n"
+    epr_generator --cross_sec_dir=$cross_section_directory --cross_sec_alias=Au --min_photon_energy=1e-3 --max_photon_energy=20.0 --min_electron_energy=1e-5 --max_electron_energy=1e5 --occupation_num_tol=1e-3 --subshell_incoherent_tol=1e-3 --grid_convergence_tol=1e-4 --grid_absolute_diff_tol=1e-80 --grid_absolute_dist_tol=1e-18 --tabular_evaluation_tol=1e-15 --cutoff_angle_cosine=0.9 --number_of_moment_preserving_angles=2.0 --linlinlin_interp_on --modify_cs_xml_file --subdir="linlin" --output_alias="LinLin" --notes="$notes"
+    if [ $? -eq 0 ]
+    then
+        printf "Au native data updated successfully!\n\n"
+    else
+        printf "Au native data FAILED to update!\n"
+        exit 1
+    fi
+else
+    printf "\nERROR: Invalid cross section directory!\n"
+    printf "  update_test_files.sh -d cross_sectin_directory\n\n"
+fi
+
