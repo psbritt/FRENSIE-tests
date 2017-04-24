da891c3f6ba039863c737d035c4551c5da496d9e
Luke Kersting <lkersting@wisc.edu>
2017-04-24 14:54:05 -0500

    Made path to epr_generator relative in hanson test file updater script.

--- hanson/update_test_files.sh
+++ hanson/update_test_files.sh
@@ -15,16 +15,17 @@ do case "${option}"
    esac
 done
 
+epr="../../bin/epr_generator"
 if [ -d "$cross_section_directory" ]; then
     
     # Update Gold Log data
     printf "Updating the Au LinLog native test data...\n"
-    epr_generator --cross_sec_dir=$cross_section_directory --cross_sec_alias=Au --min_photon_energy=1e-3 --max_photon_energy=20.0 --min_electron_energy=1e-5 --max_electron_energy=1e5 --occupation_num_tol=1e-3 --subshell_incoherent_tol=1e-3 --grid_convergence_tol=1e-4 --grid_absolute_diff_tol=1e-80 --grid_absolute_dist_tol=1e-18 --tabular_evaluation_tol=1e-15 --cutoff_angle_cosine=0.9 --number_of_moment_preserving_angles=2.0 --linlinlog_interp_on --modify_cs_xml_file --subdir="native" --output_alias="Native" --notes="$notes"
+    $epr --cross_sec_dir=$cross_section_directory --cross_sec_alias=Au --min_photon_energy=1e-3 --max_photon_energy=20.0 --min_electron_energy=1e-5 --max_electron_energy=1e5 --occupation_num_tol=1e-3 --subshell_incoherent_tol=1e-3 --grid_convergence_tol=1e-4 --grid_absolute_diff_tol=1e-80 --grid_absolute_dist_tol=1e-18 --tabular_evaluation_tol=1e-15 --cutoff_angle_cosine=0.9 --number_of_moment_preserving_angles=2.0 --linlinlog_interp_on --modify_cs_xml_file --subdir="native" --output_alias="Native" --notes="$notes"
     if [ $? -eq 0 ]
     then
-        printf "Au native data updated successfully!\n\n"
+        printf "Au native lin-lin-log data updated successfully!\n\n"
     else
-        printf "Au native data FAILED to update!\n"
+        printf "Au native lin-lin-log data FAILED to update!\n"
         exit 1
     fi
     
@@ -33,9 +34,9 @@ if [ -d "$cross_section_directory" ]; then
     epr_generator --cross_sec_dir=$cross_section_directory --cross_sec_alias=Au --min_photon_energy=1e-3 --max_photon_energy=20.0 --min_electron_energy=1e-5 --max_electron_energy=1e5 --occupation_num_tol=1e-3 --subshell_incoherent_tol=1e-3 --grid_convergence_tol=1e-4 --grid_absolute_diff_tol=1e-80 --grid_absolute_dist_tol=1e-18 --tabular_evaluation_tol=1e-15 --cutoff_angle_cosine=0.9 --number_of_moment_preserving_angles=2.0 --linlinlin_interp_on --modify_cs_xml_file --subdir="linlin" --output_alias="LinLin" --notes="$notes"
     if [ $? -eq 0 ]
     then
-        printf "Au native data updated successfully!\n\n"
+        printf "Au native lin-lin-lin data updated successfully!\n\n"
     else
-        printf "Au native data FAILED to update!\n"
+        printf "Au native lin-lin-lin data FAILED to update!\n"
         exit 1
     fi
 else
