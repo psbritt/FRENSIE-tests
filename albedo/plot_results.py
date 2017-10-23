0901c068646d82883172ff54a9694ef26484aefd
Luke Kersting <lkersting@wisc.edu>
2017-10-23 10:13:40 -0500

    Updated experimental resutls

--- albedo/plot_results.py
+++ albedo/plot_results.py
@@ -10,15 +10,15 @@ import argparse as ap
 
 # Set up the argument parser
 description = "This script asks for albedo data and run names which "\
-              "which it then plots."
+              "which it then plots against the experimental data from CREEP paper."
 
 parser = ap.ArgumentParser(description=description)
 
 
-experimental_msg = "Flag to add particular experimental data to the generated plot."
+experimental_msg = "Flag to add CREEP experimental data to the generated plot."
 parser.add_argument('-e', help=experimental_msg, action='store_true')
 
-all_experimental_msg = "Flag to add all  experimental data to the generated plot."
+all_experimental_msg = "Flag to add all experimental data to the generated plot."
 parser.add_argument('-a', help=all_experimental_msg, action='store_true')
 
 output_msg = "The output file name."
@@ -54,28 +54,32 @@ for n in range(len(file_paths)):
 fig = plt.figure(num=1, figsize=(10,5))
 plt.xlabel('Energy (MeV)', size=14)
 plt.ylabel('Reflection Coef.', size=14)
-plt.title('Electron Albedos for an infinite slab of Al}$', size=16)
+plt.title('Electron Albedos for an infinite slab of Al', size=16)
 ax=plt.gca()
 
-#plt.xlim(0.0,10.0)
+plt.xlim(0.0,.11)
 #plt.ylim(0.0,0.05)
 
 if user_args.e:
     # Get experimental data
+    data = np.loadtxt("./creep_experimental.txt", skiprows=2)
+    plt.scatter(data[:,0], data[:,1], label="CREEP", marker='s' )
+
+if user_args.a:
     data = np.loadtxt("./experimental_reflections_2.txt", skiprows=2)
-    plt.scatter(data[:,0], data[:,1], label="Neubert", marker='s' )
-    plt.scatter(data[:,0], data[:,2], label="Bishop", marker='o' )
-    # plt.scatter(data[:,0], data[:,3], label="Joy Ref. 2" )
-    # plt.scatter(data[:,0], data[:,4], label="Joy Ref. 4" )
-    # plt.scatter(data[:,0], data[:,5], label="Joy Ref. 5" )
-    # plt.scatter(data[:,0], data[:,6], label="Joy Ref. 6" )
-    # plt.scatter(data[:,0], data[:,7], label="Joy Ref. 14" )
-    # plt.scatter(data[:,0], data[:,8], label="Joy Ref. 15" )
-    # plt.scatter(data[:,0], data[:,9], label="Joy Ref. 22" )
-    # plt.scatter(data[:,0], data[:,10], label="Joy Ref. 35" )
-    # plt.scatter(data[:,0], data[:,11], label="Joy Ref. 68" )
-    # plt.scatter(data[:,0], data[:,12], label="Joy Ref. 106" )
-    # plt.scatter(data[:,0], data[:,13], label="Joy Ref. 107" )
+    plt.scatter(data[:,0], data[:,1], label="Neubert" )
+    plt.scatter(data[:,0], data[:,2], label="Bishop" )
+    plt.scatter(data[:,0], data[:,3], label="Joy Ref. 2" )
+    plt.scatter(data[:,0], data[:,4], label="Joy Ref. 4" )
+    plt.scatter(data[:,0], data[:,5], label="Joy Ref. 5" )
+    plt.scatter(data[:,0], data[:,6], label="Joy Ref. 6" )
+    plt.scatter(data[:,0], data[:,7], label="Joy Ref. 14" )
+    plt.scatter(data[:,0], data[:,8], label="Joy Ref. 15" )
+    plt.scatter(data[:,0], data[:,9], label="Joy Ref. 22" )
+    plt.scatter(data[:,0], data[:,10], label="Joy Ref. 35" )
+    plt.scatter(data[:,0], data[:,11], label="Joy Ref. 68" )
+    plt.scatter(data[:,0], data[:,12], label="Joy Ref. 106" )
+    plt.scatter(data[:,0], data[:,13], label="Joy Ref. 107" )
 
 markers = ["v","^","<",">","+","x","1","2","3","4","8","p","P","*","h","H","X","D","d"]
 markerssizes = [6,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6]
@@ -88,7 +92,7 @@ plt.legend(loc=1)
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 #ax.xaxis.set_major_formatter(FormatStrFormatter('%.4f'))
 
-output = "hanson_results.pdf"
+output = "albedo_results.pdf"
 if user_args.o:
     output = user_args.o
 
