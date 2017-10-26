d1b5010cc82172ff4d4377edc14244e516bcb92c
Luke Kersting <lkersting@wisc.edu>
2017-10-26 09:15:21 -0500

    update

--- albedo/data_combiner.py
+++ albedo/data_combiner.py
@@ -24,28 +24,31 @@ file_paths = user_args.input_files
 # Number of files
 N = len(file_paths)
 
-# Get computational results
-data = np.zeros((3,N))
-for n in range(N):
-    data[:,n] = np.loadtxt(file_paths[n], skiprows=1, dtype=str)
+if N == 0:
+  print "No files albedo specified!"
+else:
+  # Get computational results
+  data = np.zeros((3,N))
+  for n in range(N):
+      data[:,n] = np.loadtxt(file_paths[n], skiprows=1, dtype=str)
 
-# Get the sorted indices
-ind = np.lexsort(data)
+  # Get the sorted indices
+  ind = np.lexsort(data)
 
-# Create the output file
-outputfile = "combined_data.txt"
-if user_args.o:
-    outputfile = user_args.o
+  # Create the output file
+  outputfile = "combined_data.txt"
+  if user_args.o:
+      outputfile = user_args.o
 
-f = open(outputfile, 'w')
-f.write( "#Energy\tAlbedo\tError\n" )
+  f = open(outputfile, 'w')
+  f.write( "#Energy\tAlbedo\tError\n" )
 
-for i in ind:
-    line = str(data[0,i])
-    line = line + ' ' + str(data[1,i])
-    line = line + ' ' + str(data[2,i])
-    line = line + '\n'
+  for i in ind:
+      line = str(data[0,i])
+      line = line + ' ' + str(data[1,i])
+      line = line + ' ' + str(data[2,i])
+      line = line + '\n'
 
-    f.write( line )
+      f.write( line )
 
-print "Combined data saved to: ",outputfile
+  print "Combined data saved to: ",outputfile
