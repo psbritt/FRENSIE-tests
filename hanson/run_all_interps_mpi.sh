633372d7a2617804b2cbe1a193b969f09427dda6
Luke Kersting <lkersting@wisc.edu>
2017-11-08 11:06:03 -0600

    updated run scripts

--- hanson/run_all_interps_mpi.sh
+++ hanson/run_all_interps_mpi.sh
@@ -9,8 +9,8 @@
 
 # interpolations to run for mpi
 interps=( linlinlin linlinlog logloglog )
-unit_based=( false true )
-# file type (1 = Native, 2 = Moment Preserving, 3 = ACE EPR14, 4 = ACE EPR12)
+sample_policy=( correlated exact )
+# file type (1 = Native, 2 = ACE EPR14, 3 = ACE EPR12)
 file_type=1
 
 # loop through interps and run mpi script
@@ -19,14 +19,14 @@ do
     # Change the interp
     command=s/INTERP=.*/INTERP=\"$i\"/
     sed -i $command run_hanson_mpi.sh
-    # Set unit based on/off
-    for j in "${unit_based[@]}"
+    # Set 2D sampling policy
+    for j in "${sample_policy[@]}"
     do
-        command=s/UNIT_BASED_ON=.*/UNIT_BASED_ON=\"$j\"/
+        command=s/SAMPLE=.*/SAMPLE=\"$j\"/
         sed -i $command run_hanson_mpi.sh
-        if [ ${i} != linlinlog ] || [ ${j} != false ];
+        if [ ${i} != linlinlog ] || [ ${j} != exact ];
         then
-            echo -e "\nRunning Native Analog with "$i" and unit based on = "$j
+            echo -e "\nRunning Native Analog with "$i" "$j" Sampling!"
             sbatch run_hanson_mpi.sh $file_type
         fi
     done
