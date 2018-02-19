d52603d5da772ca6d5561fbcb310678b6cdb5c94
Luke Kersting <lkersting@wisc.edu>
2018-02-19 10:33:07 -0600

    Started adding a new Brem test.

---
+++ 0.75Brems/run_facemc_hanson.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+##---------------------------------------------------------------------------##
+## FACEMC test runner
+##---------------------------------------------------------------------------##
+## Validation runs comparing FRENSIE and MCNP.
+## The electron angular distribution for a thin gold foil of .0009658 cm.
+## The # of particles per steradian for scattering angle is found by dividing
+## the surface current by 2pi * ( \mu_{i} - \mu_{i-1} ) where \mu_{0} is the
+## lowest cosine bin (ie: -1). Surface current is needed so DagMC will be used.
+## The #/steradians can be changed to #/square degree by multiplying by
+## (pi/180)^2.
+## FRENSIE will be run with three variations.
+## 1. Using the Native data in analog mode, whcih uses a different interpolation
+## scheme than MCNP.
+## 2. Using Native data in moment preserving mode, which should give a less
+## acurate answer while decreasing run time.
+## 3. Using ACE (EPR14) data, which should match MCNP6.2 almost exactly.
+## 3. Using ACE (EPR12) data, which should match MCNP6.1 almost exactly.
+##---------------------------------------------------------------------------##
+
+# Set cross_section.xml directory path.
+EXTRA_ARGS=$@
+#CROSS_SECTION_XML_PATH=/home/lkersting/mcnpdata/
+CROSS_SECTION_XML_PATH=/home/software/mcnp6.2/MCNP_DATA
+FRENSIE=/home/lkersting/frensie
+
+THREADS="12"
+if [ "$#" -eq 1 ]; then
+    # Set the number of threads used
+    THREADS="$1"
+fi
+
+# Changing variables
+# Number of histories
+HISTORIES="100"
+# Turn certain reactions on (true/false)
+ELASTIC_ON="true"
+BREM_ON="true"
+IONIZATION_ON="true"
+EXCITATION_ON="true"
+# Two D Interp Policy (logloglog, linlinlin, linlinlog)
+INTERP="logloglog"
+# Two D Sampling Policy (1 = unit-base correlated, 2 = correlated, 3 = unit-base)
+SAMPLE=1
+# Elastic distribution ( Decoupled, Coupled, Hybrid )
+DISTRIBUTION="Decoupled"
+# Elastic coupled sampling method ( Simplified, 1D, 2D )
+COUPLED_SAMPLING="Simplified"
+
+ELEMENT="Au"
+ENERGY="15.7"
+NAME="native"
+
+ELASTIC="-d ${DISTRIBUTION} -c ${COUPLED_SAMPLING}"
+REACTIONS="-t ${ELASTIC_ON} -b ${BREM_ON} -i ${IONIZATION_ON} -a ${EXCITATION_ON}"
+SIM_PARAMETERS="-e ${ENERGY} -n ${HISTORIES} -s ${SAMPLE} -l ${INTERP} ${REACTIONS} ${ELASTIC}"
+
+echo -n "Enter the desired data type (1 = Native, 2 = ACE EPR14, 3 = ACE EPR12) > "
+read INPUT
+if [ ${INPUT} -eq 2 ]; then
+    # Use ACE EPR14 data
+    NAME="epr14"
+    echo "Using ACE EPR14 data!"
+elif [ ${INPUT} -eq 3 ]; then
+    # Use ACE EPR12 data
+    NAME="ace"
+    echo "Using ACE EPR12 data!"
+elif [ ${DISTRIBUTION} = "Hybrid" ]; then
+    # Use Native moment preserving data
+    NAME="moments"
+    echo "Using Native Moment Preserving data!"
+else
+    # Use Native analog data
+    echo "Using Native analog data!"
+fi
+
+NAME_EXTENTION=""
+NAME_REACTION=""
+# Set the file name extentions
+if [ "${ELASTIC_ON}" = "false" ]; then
+    NAME_REACTION="${NAME_REACTION}_no_elastic"
+elif [ ${DISTRIBUTION} = "Coupled" ]; then
+    NAME_EXTENTION="${NAME_EXTENTION}_${COUPLED_SAMPLING}"
+fi
+if [ "${BREM_ON}" = "false" ]; then
+    NAME_REACTION="${NAME_REACTION}_no_brem"
+fi
+if [ "${IONIZATION_ON}" = "false" ]; then
+    NAME_REACTION="${NAME_REACTION}_no_ionization"
+fi
+if [ "${EXCITATION_ON}" = "false" ]; then
+    NAME_REACTION="${NAME_REACTION}_no_excitation"
+fi
+
+# .xml file paths.
+echo ${SIM_PARAMETERS}
+INFO=$(python ../sim_info.py ${SIM_PARAMETERS} 2>&1)
+MAT=$(python ../mat.py -n ${ELEMENT} -t ${NAME} -i ${INTERP} 2>&1)
+SOURCE="source.xml"
+EST="est.xml"
+GEOM="geom.xml"
+RSP="../rsp_fn.xml"
+
+# Make directory for the test results
+TODAY=$(date +%Y-%m-%d)
+
+if [ ${NAME} = "ace" -o ${NAME} = "epr14" ]; then
+    DIR="results/testrun/${NAME}"
+    NAME="hanson_${NAME}${NAME_REACTION}"
+else
+    DIR="results/testrun/${INTERP}"
+
+    if [ ${SAMPLE} = 1 ]; then
+        SAMPLE_NAME="unit_correlated"
+    elif [ ${SAMPLE} = 2 ]; then
+        SAMPLE_NAME="correlated"
+    elif [ ${SAMPLE} = 3 ]; then
+        SAMPLE_NAME="unit_base"
+    fi
+
+    NAME="hanson_${NAME}_${INTERP}_${SAMPLE_NAME}${NAME_EXTENTION}${NAME_REACTION}"
+fi
+
+mkdir -p $DIR
+
+echo "Running Facemc Hanson test with ${HISTORIES} particles on ${THREADS} threads:"
+RUN="${FRENSIE}/bin/facemc --sim_info=${INFO} --geom_def=${GEOM} --mat_def=${MAT} --resp_def=${RSP} --est_def=${EST} --src_def=${SOURCE} --cross_sec_dir=${CROSS_SECTION_XML_PATH} --simulation_name=${NAME} --threads=${THREADS}"
+echo ${RUN}
+
+#gdb --args ${RUN}
+#valgrind --tool=memcheck --leak-check=yes ${RUN}
+${RUN}
+
+echo "Removing old xml files:"
+rm ${INFO} ${MAT} ../ElementTree_pretty.pyc
+
+echo "Processing the results:"
+H5=${NAME}.h5
+NEW_NAME="${DIR}/${H5}"
+NEW_RUN_INFO="${DIR}/continue_run_${NAME}.xml"
+mv ${H5} ${NEW_NAME}
+mv continue_run.xml ${NEW_RUN_INFO}
+
+cd ${DIR}
+
+bash ../../../data_processor.sh ${NAME}
+echo "Results will be in ./${DIR}"
