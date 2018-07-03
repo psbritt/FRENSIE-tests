75d5647337b8508e821420f48d0b417903405b54
Luke Kersting <lkersting@wisc.edu>
2018-07-03 14:28:29 -0500

    albedo all energy runs.

--- albedo/al/run_al_all_energies.sh
+++ albedo/al/run_al_all_energies.sh
@@ -15,13 +15,14 @@ element="Al"
 
 if [ "${element}" == "Al" ]; then
     # Source energies for Al (MeV)
+    # Creep paper source energies
     energies=( .005 .0093 .01 .011 .0134 .015 .0173 .02 .0252 .03 .04 .0415 .05 .06 .0621 .0818 .102 )
     # Assad + Bronstien source energies
-    energies=(0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0008 0.001 0.0015 0.002 0.0025 0.003 0.0035 0.004 0.0045 0.005 0.006)
+    energies=(0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0008 0.001 0.0015 0.002 0.0025 0.003 0.0035 0.004 0.0045 0.006)
     # Soum source energies
     energies=( .121 .146 .172 .196 .220 .238 .256 )
     # Low source energies (use cutoff of 20 eV)
-    energies=(0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0008 0.001 )
+    energies=(0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0008 0.001 0.002 0.003 )
 else
     echo "Error: Element \"${element}\" is currently not supported!"
 fi
--- albedo/al/run_mcnp_all_energies.sh
+++ albedo/al/run_mcnp_all_energies.sh
@@ -26,6 +26,18 @@ fi
 energies=(.005 .0093 .01 .011 .0134 .015 .0173 .02 .0252 .03 .04 .0415 .05 .06 .0621 .0818 .102)
 energies=(.005 .01 .015 .02 .03 .04 .05 .06)
 
+# Source energies for Al (MeV)
+# Creep paper source energies
+energies=( .005 .0093 .01 .011 .0134 .015 .0173 .02 .0252 .03 .04 .0415 .05 .06 .0621 .0818 .102 )
+# Assad + Bronstien source energies
+energies=(0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0008 0.001 0.0015 0.002 0.0025 0.003 0.0035 0.004 0.0045 0.006)
+# Soum source energies
+energies=( .121 .146 .172 .196 .220 .238 .256 )
+# Low source energies (use cutoff of 20 eV)
+energies=(0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0008 0.001 0.002 0.003 )
+
+energies=(.0093 .011 .0134 .0173 .0252 .0415 .0621 .0818 .102)
+
 # Set the input file name
 NAME="mcnp.in"
 mkdir -p $OUTPUT_DIR
