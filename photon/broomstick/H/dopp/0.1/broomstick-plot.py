4bc18f4196e73db260939a0dc35fb5e61cb53f9f
Alex Robinson <aprobinson@wisc.edu>
2018-10-19 18:26:40 -0500

    Updated the broomstick/H/dopp/0.1 problem plot using the updated results from the campus cluster.

--- photon/broomstick/H/dopp/0.1/broomstick-plot.py
+++ photon/broomstick/H/dopp/0.1/broomstick-plot.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     if options.is_a_current:
         top_ylims = [0.0, 60.0]
-        bottom_ylims = [0.90, 1.10]
+        bottom_ylims = [0.98, 1.02]
         legend_pos = (0.86,0.76)
     else:
         top_ylims = [0.0, 500000.0]
