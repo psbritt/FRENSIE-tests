5d393a2f69634d49472cb599fdba281689d7b77d
Alex Robinson <aprobinson@wisc.edu>
2019-03-15 12:14:40 -0500

    Added additional photon/broomstick/H simulations.

---
+++ photon/broomstick/H/nodopp_ia/0.5/broomstick.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This file is named broomstick.sh
+#SBATCH --partition=pre
+#SBATCH --time=0-06:00:00
+#SBATCH --ntasks=40
+#SBATCH --cpus-per-task=4
+
+mpirun -np $SLURM_NTASKS python2.7 broomstick.py --db_path=$DATABASE_PATH --sim_name="broomstick" --num_particles=1e8 --threads=$SLURM_CPUS_PER_TASK
